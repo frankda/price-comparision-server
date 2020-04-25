f391baa4f831381955ef486fcb73d125da9edb0d
frankda <36907108+frankda@users.noreply.github.com>
2020-04-25 15:59:44 +1000

    make ts working

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,17 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => console.log('listening http://localhost:' + port))
+
+// const express = require( "express" );
+// const app = express();
+// const port = 8080; // default port to listen
+
+// // define a route handler for the default home page
+// app.get( "/", ( req, res ) => {
+//     res.send( "Hello world!" );
+// } );
+
+// // start the Express server
+// app.listen( port, () => {
+//     console.log( `server started at http://localhost:${ port }` );
+// } );
