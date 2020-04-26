b39bf3e38d87795be7112a7dc632a438b80f3107
frankda <36907108+frankda@users.noreply.github.com>
2020-04-26 19:09:25 +1000

    server backbone finished

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,23 @@
 import express from 'express';
 import mongoose from "mongoose";
-import { Product } from './models/products';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 import { initTestData } from './controllers/productController';
 
-// global.Product = require('./models/products');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect mongoDB Atlas
+// Connect Mongodb atlas and check if connected
 mongoose.connect(`mongodb+srv://frank:dqx910418@cluster0-fxva0.mongodb.net/test?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true })
-
-// Check if database has been connected
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-// Load middleware
+// Express configuration
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors());
+app.set("port", process.env.PORT || 3000);
 
 /**
  * Primary app routes 
@@ -29,4 +25,6 @@ app.use(cors());
 
 app.get('/', initTestData)
 
-app.listen(port, () => console.log('listening http://localhost:' + port))
+// app.listen(port, () => console.log('listening http://localhost:' + port))
+
+export default app
