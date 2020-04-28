f0737019284e464f26e5e127d95519876b4a02e8
frankda <suyunyi@gmail.com>
2020-04-28 22:27:52 +1000

    handle error

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { Product } from '../models/products';
 import { Request, Response, NextFunction } from "express";
 import { searchChemistProduct } from '../crawler/productCrawler';
+import cheerio from 'cheerio';
 
 // Init seed data for testing purpose
 export const initTestData = (req: Request, res: Response, next: NextFunction) => {
@@ -13,9 +14,22 @@ export const initTestData = (req: Request, res: Response, next: NextFunction) =>
   // next();  // delete this if there is no next function, or will cause ERR_HTTP_HEADERS_SENT
 }
 
-export const searchProduct = async (req: Request, res: Response) => {
-  const productname = req.body.productname;
-  const html = await searchChemistProduct(productname);
-  console.log(html)
-  res.json(html);
+export const searchProduct = async (req: Request, res: Response, next: NextFunction) => {
+  // Return products from different stores to front-end
+  const chemistProducts = [];
+  const pricelineProducts = [];
+
+  const productname = req.body.productname;   // get the search value from front-end
+  const result = await searchChemistProduct(productname);   // next() will be called with thrown error or the rejected value
+  const html = result.text  // if the result success 
+  if (!html.includes('product-name')) res.send('No matching were found');   // if cannot find matching products
+
+  // use cheerio to parse search results
+  console.log('html page obtained')
+  const $ = cheerio.load(html);
+  const searchedProductsFromChemist = $("a[class='product-container search-result']");
+  console.log(searchedProductsFromChemist.eq(0).html());
+
+  
+  res.send($("div[class=product-name]").eq(0).text());
 }
--- src/crawler/productCrawler.ts
+++ src/crawler/productCrawler.ts
@@ -1,7 +1,20 @@
 import request from 'superagent';
 import { Request, Response, NextFunction } from "express";
 
+/*
+* These codes are for errors
+*/
+// export async function searchChemistProduct (next: NextFunction, productname: String) {
+//   try {
+//     const result = await request.get(`https://www.chemistwarehouse.com.au/search?searchtext=${productname}&searchmode=allwords`)
+//     return result.text
+//   } catch (err) {
+//     console.log('Something is going wrong when searching chemist products, check /crawler/productCrawler.ts for information');
+//     console.error(err.message);
+//     return 'error';
+//   }
+// }
+
 export async function searchChemistProduct (productname: String) {
-  const result = await request.get(`https://www.chemistwarehouse.com.au/search?searchtext=${productname}&searchmode=allwords`)
-  return result.text
+  return await request.get(`https://www.chemistwarehouse.com.au/search?searchtext=${productname}&searchmode=allwords`)
 }
